c272a8db71df692251dee306c4be0800c915d0df
Raymond Cvoz <sranchvook@outlook.com>
2022-12-26 21:11:49 +0800

    Update ExamParticipantMapper.java

--- src/main/java/etms/web/mapper/ExamParticipantMapper.java
+++ src/main/java/etms/web/mapper/ExamParticipantMapper.java
@@ -16,58 +16,58 @@ import java.util.List;
 public interface ExamParticipantMapper
 {
     /**
-     * 获取某个考试的参赛人数.
+     * 获取某个考试的参加人数.
      *
      * @param examId - 考试的唯一标识符
-     * @return 某个考试的参赛人数
+     * @return 某个考试的参加人数
      */
     long getNumberOfParticipantsOfExam(long examId);
 
     /**
-     * 获取某个考试的参赛者列表
+     * 获取某个考试的参加者列表
      *
      * @param examId - 考试的唯一标识符
-     * @return 某个考试的参赛者列表
+     * @return 某个考试的参加者列表
      */
     List<ExamParticipant> getParticipantsOfExam(
             @Param("examId") long examId);
 
 
     /**
-     * 获取某个选手在某个考试的参赛记录.
+     * 获取某个选手在某个考试的参加记录.
      *
      * @param examId     - 考试的唯一标识符
-     * @param uid - 参赛者的用户唯一标识符
-     * @return 对应的某个选手在某个考试的参赛记录
+     * @param uid - 参加者的用户唯一标识符
+     * @return 对应的某个选手在某个考试的参加记录
      */
     ExamParticipant getParticipantOfExam(
             @Param("examId") long examId, @Param("uid") long uid);
 
     /**
-     * 创建参赛记录(用于参加考试).
+     * 创建参加记录(用于参加考试).
      *
-     * @param examParticipant - 待创建的参赛记录
+     * @param examParticipant - 待创建的参加记录
      */
     int createExamParticipant(ExamParticipant examParticipant);
 
     /**
-     * 更新参赛记录. 用于参加OI考试, 更新提交的源代码.
+     * 更新参加记录. 用于参加OI考试, 更新提交的源代码.
      *
-     * @param examParticipant - 待更新的参赛记录
+     * @param examParticipant - 待更新的参加记录
      */
     int updateExamParticipant(ExamParticipant examParticipant);
 
     /**
-     * 删除参赛记录.
+     * 删除参加记录.
      *
      * @param examId     - 考试的唯一标识符
-     * @param participantUid - 参赛者的用户唯一标识符
+     * @param participantUid - 参加者的用户唯一标识符
      */
     int deleteExamParticipant(
             @Param("examId") long examId, @Param("participantUid") long participantUid);
 
     /**
-     * 删除比赛所有参赛记录
+     * 删除比赛所有参加记录
      * @param examId 考试的唯一标识符
      * @return
      */
