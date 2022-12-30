6fa759198c3740e29da6456d9e277ad486d4536b
Raymond Cvoz <sranchvook@outlook.com>
2022-12-30 09:55:00 +0800

    更新数据库

--- src/main/java/etms/web/controller/DefaultController.java
+++ src/main/java/etms/web/controller/DefaultController.java
@@ -1,6 +1,5 @@
 package etms.web.controller;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
